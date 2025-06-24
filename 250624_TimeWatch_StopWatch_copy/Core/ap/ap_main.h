6dde75e139d01e25997b8db28f8ba48b3e0a1669
Heeju99 <limheeju99@naver.com>
2025-06-24 11:03:15 +0900

    dot_prof

---
+++ 250624_TimeWatch_StopWatch_copy/Core/ap/ap_main.h
@@ -0,0 +1,14 @@
+#ifndef AP_AP_MAIN_H_
+#define AP_AP_MAIN_H_
+#include "LedBar.h"
+#include "Button.h"
+#include "FND_prof.h"
+#include "stm32f4xx_hal.h"
+#include "tim.h" //timer Handler
+#include "stopWatch.h"
+#include "timeWatch.h"
+
+void ap_Init();
+int ap_main();
+
+#endif /* AP_AP_MAIN_H_ */
---
+++ 250624_TimeWatch_StopWatch_copy/Core/driver/LedBar/LedBar.h
@@ -0,0 +1,9 @@
+#ifndef DRIVER_LEDBAR_LEDBAR_H_
+#define DRIVER_LEDBAR_LEDBAR_H_
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
+#include "stm32f411xe.h"
+
+void LEDBar_Write(uint8_t data);
+
+#endif /* DRIVER_LEDBAR_LEDBAR_H_ */
